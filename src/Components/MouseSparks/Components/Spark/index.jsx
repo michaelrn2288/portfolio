bddecdd00ac3b09a1eb81509e1dd4f982e05be31
Michael Rocha Nunes <michaelrn2288@gmail.com>
2022-12-31 19:19:39 -0300

    improve sparks effect

--- src/Components/MouseSparks/Components/Spark/index.jsx
+++ src/Components/MouseSparks/Components/Spark/index.jsx
@@ -5,30 +5,29 @@ import { randomNumber } from '../../../../Utils/random'
 const SparkAnimation = keyframes`
     from{
         transform: translateY(0);
-        opacity: 1;
+        opacity: 0.9;
     }
     to {
-        transform: translateY(200px);
+        transform: translateY(220px);
         opacity: 0;
     }
 `
 
 const StyledSpark = styled.div`
     position: fixed;
-    width: 11px;
-    height: 11px;
-    border-radius: 50%;
+    z-index: 99;
+    width: 13px;
+    height: 13px;
+    clip-path: polygon(50% 0, 64% 35%, 100% 50%, 64% 64%, 50% 100%, 35% 64%, 0 50%, 35% 35%);
     background-image: radial-gradient(circle,
         rgba(255,255,255,1) 0%,
-        rgba(255,255,122,1) 45%,
-        rgba(245,255,0,1) 75%,
-        rgba(255,190,0,1) 100%);
-    box-shadow: 0 0 15px 4px rgba(255, 190, 0, 0.5);
+        rgba(255,255,122, 1) 45%,
+        rgba(245,255,0, 1) 75%,
+        rgba(255,190,0, 1) 100%);
     left: ${props => props.initialPosition.x}px;
     top: ${props => props.initialPosition.y}px;
-    animation-name: ${SparkAnimation};
-    animation-duration: 1.3s;
-    animation-fill-mode: forwards;
+    animation: ${SparkAnimation} 1.1s ease-in forwards;
+
 `
 
 export default function Spark(props) {
