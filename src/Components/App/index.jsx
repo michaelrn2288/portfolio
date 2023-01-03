6af216a990f706e524e8b734d8b12450c2cb778a
Michael Rocha Nunes <michaelrn2288@gmail.com>
2023-01-02 23:32:14 -0300

    add color picker and state to store color selected

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { hexToHSL } from "../../Utils/colorConverter";
+import ColorSelector from "../ColorSelector";
 import Footer from "../Footer";
 import Header from "../Header";
 import MouseSparks from "../MouseSparks";
@@ -12,8 +14,19 @@ const StyledMain = styled.main`
     background-color:aquamarine;
 `
 
+const CustomColor = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 10%;
+    height: 66px;
+    width: 66px;
+    background-color: ${ props => props.selectedColor.hslcolor };
+    border: solid black 7px;
+`
+
 export default function App() {
     const [areSparksOn, setAreSparksOn] = useState(true)
+    const [selectedColor, setSelectedColor] = useState({ hexcolor: '#ab0b0b', hslcolor: '' })
 
     const headerHeight = '10'//vh
 
@@ -29,6 +42,13 @@ export default function App() {
                 textWhenOn={'desligar faíscas'}
                 textWhenOff={'ligar faíscas'}
             />
+            <ColorSelector
+                selectedColor={selectedColor}
+                setSelectedColor={setSelectedColor}
+            />
+            <CustomColor
+                selectedColor={selectedColor}
+            ></CustomColor>
             <StyledMain>
                 <Router headerHeight={headerHeight} />
             </StyledMain>
---
+++ src/Components/ColorSelector/index.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+import { hexToHSL } from "../../Utils/colorConverter"
+
+const ColorInput = styled.input`
+    position: fixed;
+`
+
+export default function ColorSelector(props) {
+
+    function handleChange(event) {
+        const { name, value } = event.target
+        props.setSelectedColor(prevColor => ({
+            [name]: value,
+            hslcolor: hexToHSL(value)
+        }))
+    }
+
+    return (
+        <>
+            <ColorInput
+                type='color'
+                name='hexcolor'
+                value={props.selectedColor.hexcolor}
+                onChange={handleChange}
+            />
+        </>
+    )
+}
