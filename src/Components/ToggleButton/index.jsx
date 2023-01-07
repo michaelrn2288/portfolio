d84284b9fe3715458d3f1aae089b9a10496685b6
Michael Rocha Nunes <michaelrn2288@gmail.com>
2023-01-06 23:38:19 -0300

    add controller to hide and show button

--- src/Components/ToggleButton/index.jsx
+++ src/Components/ToggleButton/index.jsx
@@ -1,9 +1,38 @@
-import styled from "styled-components"
+import { useState } from "react"
+import styled, { keyframes } from "styled-components"
+
+const hiddenButtonSpot = '-220px'
+
+const slideInButton = keyframes`
+    from {
+        transform: translateX(${hiddenButtonSpot})
+    }
+    to {
+        transform: translateX(0);
+    }
+`
+
+const slideOutButton = keyframes`
+    from {
+        transform: translateX(0);
+    }
+    to {
+        transform: translateX(${hiddenButtonSpot});
+    }
+`
+
+const MainComponent = styled.div`
+    display: flex;
+    position: fixed;
+    gap: 20px;
+    top: 380px;
+    left: 30px;
+    transform: ${hiddenButtonSpot};
+    animation: ${props => (
+        props.isComponentRevealed ? slideInButton : slideOutButton)} 0.4s linear forwards;
+`
 
 const StyledButton = styled.button`
-    position: fixed;
-    top: 48vh;
-    left: 2vw;
     padding: 10px;
     border: none;
     border-radius: 20px;
@@ -23,17 +52,47 @@ const StyledButton = styled.button`
     }
 `
 
+const SlideController = styled.div`
+    font-size: 3rem;
+    cursor: pointer;
+    transform: scaleX(1.5) rotateY(${props => props.arrowPosition});
+`
+
 export default function ToggleButton(props) {
+
+    const [wasComponentRevealed, setWasComponentRevealed] = useState(false)
+    const [isComponentRevealed, setIsComponentRevealed] = useState(false)
+    const [arrowPosition, setArrowPosition] = useState('180deg')
+
+    function toggleRevealComponent() {
+        setIsComponentRevealed(prevState => !prevState)
+        setArrowPosition(prevState => {
+            return prevState === '0' ? '180deg' : '0'
+        })
+        console.log(arrowPosition)
+    }
+
     function handleClick() {
         props.toggleState(prevState => !prevState)
     }
 
     return (
-        <StyledButton onClick={handleClick} >
-            {props.isSwitchOn ?
-                props.textWhenOn
-                : props.textWhenOff
-            }
-        </StyledButton>
+        <MainComponent
+        wasComponentRevealed={wasComponentRevealed}
+        isComponentRevealed={isComponentRevealed}
+        >
+            <StyledButton onClick={handleClick} >
+                {props.isSwitchOn ?
+                    props.textWhenOn
+                    : props.textWhenOff
+                }
+            </StyledButton>
+            <SlideController
+                onClick={toggleRevealComponent}
+                arrowPosition={arrowPosition}
+            >
+            ❬❬
+            </SlideController>
+        </MainComponent>
     )
 }
